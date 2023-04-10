55b35034464dfe42bc19df6636bb6dceb2a8369f
Jason Park <jjpark987@gmail.com>
2023-04-10 13:18:47 -0700

    commit tree fe546e3243d545da9ddec65580c5faaee152f86d

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+
+def badge_maker(speaker) 
+    "Hello, my name is #{speaker}."
+end
+
+def batch_badge_creator(array_of_speakers) 
+    array_of_speakers.collect do |speaker| 
+        badge_maker(speaker)
+    end
+end
+
+def assign_rooms(array_of_speakers) 
+    array_of_speakers.collect.with_index do |speaker, index| 
+        "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
+    end
+end
+
+def printer(array_of_speakers)
+    batch_badge_creator(array_of_speakers).each do |badge|
+        puts badge
+    end
+    assign_rooms(array_of_speakers).each do |room_assignment| 
+        puts room_assignment
+    end
+end
+
